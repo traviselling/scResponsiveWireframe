ccedd484f4a8f7fd2c9b08ec33b9c0dab9a8d15f
Travis Elling <ellingtravis@gmail.com>
2017-09-26 11:51:58 -0700

    this is a working HTML version of the app complete with javacript animation. Copy needs to be completed in the next version.

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
+  $('.smarty').animate({
+      left:"0px"
+  },2000, "linear");
+
+  $('h1').animate({
+      fontSize : "5em"
+  },3000);
+  $('h2').animate({
+      fontSize : "3.5em"
+  },3000);
+  $('.info').fadeIn(9000);
+  $('.contactHeader').animate({
+      fontSize : "2em"
+  },3000);
+  $('.formWrapper').animate({
+      right:"0px"
+  },2000, "linear");
+  $('.aboutSectionHeader').animate({
+      fontSize:"4em"
+  },3000);
 
 $(".logo").click(function(){
-  if ($('#nav').classname !== "hidden") {
-    $("#nav").toggleClass("hidden");
+  if ($('#nav').display !== "none") {
+    $("#nav").slideToggle("slow");
     $("#mainWindow").toggleClass("col-md-12");
     $("#mainWindow").toggleClass("col-md-9");
-
-
+      if(  $(".logo img").css('height')=== "150px"){
+          $(".logo img").css('height','100px');
+      }else if($(".logo img").css('height')=== "100px"){
+          $(".logo img").css('height','150px');
+      }
   }
 });
 
